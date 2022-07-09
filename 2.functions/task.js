74710971a1a39537444566e11062ac2f28d6620a
naka16bit <naka@local.me>
2022-07-09 22:30:04 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,55 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+    sum += arr[i];
+  }
+  
+  avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+   for (let i = 0; i < arrOfArr.length; i++) {
+    let newMax = func(arrOfArr[i]);
+    if (newMax > max) max = newMax;
+   }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, difference;
+  min = Infinity;
+  max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > max) max = arr[i];
+  }
+
+  difference = Math.abs(max - min);
+
+  return difference;
 }
