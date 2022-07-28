ac4869d12748d9d8a01beb919b4c38ebf5ea6ba8
naka16bit <naka@local.me>
2022-07-28 17:34:28 +0300

    first commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return function (...args) {
+    let hash = args.join(',');
+    let value = cache.value;
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache === undefined) {
+      cache.value = value = func.call(this, ...args);
+      cache.push({hash, value});
+      if (cache.length > 5) cache.shift();
+      console.log('Вычисляем: ' + value);
+      return 'Вычисляем: ' + value
+    } else {
+      console.log('Из кэша: ' + value);
+      return 'Из кэша: ' + value;
+    }
+  }
 }
 
+const debounceDecoratorNew = (f, ms) => {
+  let timerId;
+  let firstRunFlag = true;
+  
+  f.count = 0;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  return function (...args) {
+    if (firstRunFlag) {
+      f.apply(this, args);
+      firstRunFlag = false;
+      f.count = 1;
+      return;
+    }
 
-function debounceDecorator2(func) {
-  // Ваш код
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      f.apply(this, args);
+      console.timeEnd("time"); //(2)
+    }, ms);
+
+    f.count++;
+  }
 }
+  
+console.time("time"); //(1)
